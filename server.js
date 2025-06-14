f3c8dec02fe47d8019a932ec1c9f6ca9e90b5fe6
mouh25-cha <chabim971@gmail.com>
2025-06-14 12:06:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -143,7 +143,7 @@ app.get("/energy/all", async (req, res) => {
   try {
     const data = await EnergyModel.find()
       .sort({ timestamp: -1 })
-      .limit(2000);
+      .limit(200);
 
     res.json(data);
   } catch (error) {
